fe297885cfdd640c3361ea8ba4e77386a0f6c987
liamray <liamraymisc@gmail.com>
2017-09-25 23:08:05 +0300

    developing UI

---
+++ public/data.js
@@ -0,0 +1,70 @@
+var nexlSourceSample = [
+	{
+		icon: "images/dir.png",
+		label: "common",
+		id: 'common-id',
+		expanded: false,
+		items: [
+			{icon: "images/js-file.png", label: "interfaces.js"},
+			{icon: "images/js-file.png", label: "commons.js"},
+			{icon: "images/js-file.png", label: "error-messages.js"}
+		]
+	},
+
+	{
+		icon: "images/dir.png",
+		label: "jenkins",
+		items: [
+			{
+				icon: "images/js-file.png",
+				label: "jenkins.js"
+			},
+			{
+				icon: "images/js-file.png",
+				label: "permissions.js"
+			}
+		]
+	},
+
+	{
+		icon: "images/dir.png",
+		label: "jvm-opts",
+		expanded: false,
+		items: [
+			{
+				icon: "images/dir.png",
+				label: "app-server",
+				items: [
+					{
+						icon: "images/js-file.png",
+						label: "app-server.js"
+					}
+				]
+			},
+			{
+				icon: "images/dir.png",
+				label: "pearl",
+				items: [
+					{
+						icon: "images/js-file.png",
+						label: "pearl-type1.js"
+					},
+					{
+						icon: "images/js-file.png",
+						label: "pearl-type2.js"
+					},
+					{
+						icon: "images/js-file.png",
+						label: "pearl-type3.js"
+					}
+				]
+			}
+		]
+	},
+	{
+		icon: "images/js-file.png", label: "test.js"
+	},
+	{
+		icon: "images/general-file.png", label: "Thumbs.db"
+	}
+];
