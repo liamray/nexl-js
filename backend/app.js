0189e3c87bd9175cb97ba4e308de997a168f222b
liamray <liamraymisc@gmail.com>
2017-10-08 01:27:08 +0300

    developing UI

--- backend/app.js
+++ backend/app.js
@@ -5,14 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var nexlRest = require('./routes/nexl-rest');
+var evalExpression = require('./routes/eval-expression');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /../frontend
 //app.use(favicon(path.join(__dirname, '../frontend', 'favicon.ico')));
@@ -20,10 +16,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../frontend')));
 
-app.use('/', index);
-app.use('/users', users);
+// todo : complete it
+app.use(express.static(path.join(__dirname, '../frontend')));
+app.use('/nexl-rest/*', nexlRest);
+app.use('/*', evalExpression);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
