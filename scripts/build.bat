bca65fb834e49be6c9e70883c2945b68dbdc1716
liamray <liamraymisc@gmail.com>
2018-01-12 19:02:01 +0200

    - improving build

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,6 +1,6 @@
 rem building angular site
 cd ..\angular
-cmd /C ng build --prod --output-path ..\frontend\nexl\site
+cmd /C ng build --prod --aot --output-path ..\frontend\nexl\site
 
 rem fixing imports
 cd ..\scripts
@@ -9,8 +9,5 @@ node fix-urls.js "..\frontend\nexl\site\index.html"
 rem moving images dir
 move ..\frontend\nexl\site\nexl\site\images ..\frontend\nexl\site\images
 rmdir ..\frontend\nexl\site\nexl /S /Q
-
-rem copy favicon
-copy ..\angular\favicon.ico ..\frontend\nexl\site
 
 pause
