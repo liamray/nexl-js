9963958b3a8981bc1e4683e7dd9d174ccc853be1
liamray <liamraymisc@gmail.com>
2018-01-16 16:20:50 +0200

    - fixed [ng build --prod] bug

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,7 +1,5 @@
 rem building angular site
 cd ..\angular
-rem temporary not using --prod and --aot because of bug in jxqwidgets
-rem cmd /C ng build --prod --base-href /nexl/site/ --output-path ..\frontend\nexl\site
-cmd /C ng build --base-href /nexl/site/ --output-path ..\frontend\nexl\site
+cmd /C ng build --prod --base-href /nexl/site/ --output-path ..\frontend\nexl\site
 
 pause
