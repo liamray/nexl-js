82ae2b65eb50b40c4eb74933a19e097a30efae5b
liamray <liamraymisc@gmail.com>
2018-03-04 20:39:16 +0200

    - settings -> general ( UI )

--- angular/src/app/main/settings/settings.component.ts
+++ angular/src/app/main/settings/settings.component.ts
@@ -1,19 +1,41 @@
-import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ComponentFactoryResolver, ViewChild, ViewContainerRef} from '@angular/core';
 import {jqxWindowComponent} from "jqwidgets-scripts/jqwidgets-ts/angular_jqxwindow";
 import {jqxButtonComponent} from "jqwidgets-scripts/jqwidgets-ts/angular_jqxbuttons";
 import {BindingsComponent} from "./bindings/bindings.component";
+import {GeneralComponent} from "./general/general.component";
+import {LoggerComponent} from "./logger/logger.component";
+import {CallbacksComponent} from "./callbacks/callbacks.component";
+import {UiComponent} from "./ui/ui.component";
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.css']
 })
-export class SettingsComponent implements AfterViewInit {
+export class SettingsComponent {
   @ViewChild('settingsWindow')
   settingsWindow: jqxWindowComponent;
 
-  @ViewChild('bindings')
+  @ViewChild('general', {read: ViewContainerRef})
+  generalRef;
+
+  @ViewChild('bindings', {read: ViewContainerRef})
+  bindingsRef;
+
+  @ViewChild('logger', {read: ViewContainerRef})
+  loggerRef;
+
+  @ViewChild('callbacks', {read: ViewContainerRef})
+  callbacksRef;
+
+  @ViewChild('ui', {read: ViewContainerRef})
+  uiRef;
+
+  general: any;
   bindings: BindingsComponent;
+  logger: LoggerComponent;
+  callbacks: CallbacksComponent;
+  ui: UiComponent;
 
   @ViewChild('saveButton')
   saveButton: jqxButtonComponent;
@@ -21,15 +43,11 @@ export class SettingsComponent implements AfterViewInit {
   @ViewChild('cancelButton')
   cancelButton: jqxButtonComponent;
 
-  constructor() {
+  constructor(private componentFactoryResolver: ComponentFactoryResolver) {
   }
 
   open() {
     this.settingsWindow.open();
-  }
-
-  ngAfterViewInit() {
-    // this.settingsWindow.close();
   }
 
   save() {
@@ -38,7 +56,25 @@ export class SettingsComponent implements AfterViewInit {
   }
 
   initContent = () => {
+    jqwidgets.createInstance('#ribbon', 'jqxRibbon', {
+      width: 600,
+      height: 400,
+      position: 'left',
+      selectionMode: 'click',
+      animationType: 'none'
+    });
     this.saveButton.createComponent();
     this.cancelButton.createComponent();
+    this.general.instance.createButtons();
+
+  };
+
+  ngOnInit() {
+    this.general = this.generalRef.createComponent(this.componentFactoryResolver.resolveComponentFactory(GeneralComponent));
+    this.bindings = this.bindingsRef.createComponent(this.componentFactoryResolver.resolveComponentFactory(BindingsComponent));
+    this.logger = this.loggerRef.createComponent(this.componentFactoryResolver.resolveComponentFactory(LoggerComponent));
+    this.callbacks = this.callbacksRef.createComponent(this.componentFactoryResolver.resolveComponentFactory(CallbacksComponent));
+    this.ui = this.uiRef.createComponent(this.componentFactoryResolver.resolveComponentFactory(UiComponent));
+
   }
 }
