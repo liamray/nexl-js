bc7225d9212d45e705d051a90dd90260515936de
liamray <liamraymisc@gmail.com>
2018-03-10 21:03:47 +0200

    - nexl settings backend + frontend

--- backend/routes/settings/settings-route.js
+++ backend/routes/settings/settings-route.js
@@ -3,16 +3,17 @@ const router = express.Router();
 
 const utils = require('../../api/utils');
 const security = require('../../api/security');
+const cmdLineArgs = require('../../api/cmd-line-args');
 const settings = require('../../api/settings');
 const confMgmt = require('../../api/conf-mgmt');
 const logger = require('../../api/logger');
 
-const SETTINGS_FILTER = [settings.NEXL_SOURCES_DIR, settings.NEXL_SOURCES_ENCODING, settings.HTTP_TIMEOUT, settings.LDAP_URL, settings.HTTP_BINDING, settings.HTTP_PORT, settings.HTTPS_BINDING, settings.HTTPS_PORT, settings.SSL_CERT_LOCATION, settings.SSL_KEY_LOCATION, settings.LOG_FILE_LOCATION, settings.LOG_LEVEL, settings.LOG_ROTATE_FILE_SIZE, settings.LOG_ROTATE_FILES_COUNT, settings.NEXL_CALLBACKS];
+const SETTINGS_2_LOAD = [settings.NEXL_SOURCES_DIR, settings.NEXL_SOURCES_ENCODING, settings.HTTP_TIMEOUT, settings.LDAP_URL, settings.HTTP_BINDING, settings.HTTP_PORT, settings.HTTPS_BINDING, settings.HTTPS_PORT, settings.SSL_CERT_LOCATION, settings.SSL_KEY_LOCATION, settings.LOG_FILE_LOCATION, settings.LOG_LEVEL, settings.LOG_ROTATE_FILE_SIZE, settings.LOG_ROTATE_FILES_COUNT, settings.NEXL_CALLBACKS];
 
 function filterSettings(settings) {
 	const result = {};
 	for (let key in settings) {
-		if (SETTINGS_FILTER.indexOf(key) >= 0) {
+		if (SETTINGS_2_LOAD.indexOf(key) >= 0) {
 			result[key] = settings[key];
 		}
 	}
@@ -31,8 +32,11 @@ router.post('/load', function (req, res, next) {
 		return;
 	}
 
+	// load setting one by one according to SETTINGS_2_LOAD
+	// optimize settings.get() method -> allow to pass a whole object
 	let allSettings = confMgmt.load(confMgmt.CONF_FILES.SETTINGS);
 	let filteredSettings = filterSettings(allSettings);
+	filteredSettings['nexl-home-dir'] = cmdLineArgs.NEXL_HOME_DIR;
 	res.send(filteredSettings);
 });
 
@@ -46,6 +50,8 @@ router.post('/save', function (req, res, next) {
 		utils.sendError(res, 'admin permissions required');
 		return;
 	}
+
+	// load all settings, iterate over new items and compare with loaded ( of course filter items by SETTINGS_2_LOAD )
 
 	confMgmt.save(req.body.admins, confMgmt.CONF_FILES.ADMINS);
 	confMgmt.save(req.body.assignPermissions, confMgmt.CONF_FILES.PERMISSIONS);
