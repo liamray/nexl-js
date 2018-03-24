caebadbe2bc4d5aef2f3bf7174bb94999a45bfdb
liamray <liamraymisc@gmail.com>
2018-03-24 23:55:32 +0300

    - refactoring. all config validations and default values are moved to conf-mgmt.js - added schema-validation.js tiny validation engine

--- backend/routes/reserved/reserved-route.js
+++ backend/routes/reserved/reserved-route.js
@@ -4,7 +4,7 @@ const utils = require('../../api/utils');
 const router = express.Router();
 
 router.get('/', function (req, res, next) {
-	utils.sendError(res, '[/nexl] path is reserved for internal purposes');
+	utils.sendError(res, '[/nexl] path is reserved for internal use');
 });
 
 // --------------------------------------------------------------------------------
