5674abc545b9e6f8833a8863b1f0fb077ba44226
liamray <liamraymisc@gmail.com>
2018-04-27 19:27:25 +0300

    - enabling Ctrl+S shortcut to save nexl source

--- angular/src/app/main/main.component.ts
+++ angular/src/app/main/main.component.ts
@@ -1,5 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from "../services/auth.service";
+import * as $ from 'jquery';
+import {MESSAGE_TYPE, MessageService} from "../services/message.service";
+
+export const CTRL_S = 'control+s';
+export const F9 = 'F9';
 
 @Component({
   selector: 'app-main',
@@ -8,10 +13,45 @@ import {AuthService} from "../services/auth.service";
 })
 export class MainComponent implements OnInit {
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private messageService: MessageService) {
+  }
+
+  discoverKeyCombination(event) {
+    if (event.ctrlKey || event.metaKey) {
+      switch (String.fromCharCode(event.which).toLowerCase()) {
+        case 's':
+          return CTRL_S;
+      }
+    }
+
+    if (event.which === 120) {
+      return F9;
+    }
+
+    return null;
+  }
+
+  interceptHotKeys() {
+    $(window).bind('keydown', (event) => {
+      const key = this.discoverKeyCombination(event);
+      switch (key) {
+        case CTRL_S:
+          this.messageService.sendMessage({
+            type: MESSAGE_TYPE.SAVE_NEXL_SOURCE
+          });
+          event.preventDefault();
+          return;
+
+
+        case F9 :
+          event.preventDefault();
+          return;
+      }
+    });
   }
 
   ngOnInit() {
     this.authService.refreshStatus();
+    this.interceptHotKeys();
   }
 }
