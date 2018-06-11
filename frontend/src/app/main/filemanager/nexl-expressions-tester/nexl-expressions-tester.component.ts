306e186e9df7aa0c9859335f2a2d54e6079ff945
liamray <liamraymisc@gmail.com>
2018-06-11 19:14:52 +0300

    - updated text

--- frontend/src/app/main/filemanager/nexl-expressions-tester/nexl-expressions-tester.component.ts
+++ frontend/src/app/main/filemanager/nexl-expressions-tester/nexl-expressions-tester.component.ts
@@ -442,7 +442,7 @@ export class NexlExpressionsTesterComponent implements AfterViewInit {
 
     const opts = {
       title: 'Information',
-      label: `Please note the [${this.relativePath}] JavaScript file is not saved. You can get different result until you save it`,
+      label: `Please note the [${this.relativePath}] JavaScript file was modified. You can get different result until you save it`,
       checkBoxText: 'Don\'t show this message again',
       callback: (dontShowAgain: boolean) => {
         window.open(this.urlEncoded);
