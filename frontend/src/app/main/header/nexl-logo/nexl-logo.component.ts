44f5fa262e1e39cf95d29041baf6169340fee387
liamray <liamraymisc@gmail.com>
2018-08-02 22:33:17 +0300

    - updated nexl logo & favicon

--- frontend/src/app/main/header/nexl-logo/nexl-logo.component.ts
+++ frontend/src/app/main/header/nexl-logo/nexl-logo.component.ts
@@ -1,9 +1,16 @@
 import {Component} from "@angular/core";
+import {MESSAGE_TYPE, MessageService} from "../../services/message.service";
 
 @Component({
-	selector: '.app-nexl-logo',
-	templateUrl: './nexl-logo.component.html',
-	styleUrls: ['./nexl-logo.component.css']
+  selector: '.app-nexl-logo',
+  templateUrl: './nexl-logo.component.html',
+  styleUrls: ['./nexl-logo.component.css']
 })
 export class NexlLogoComponent {
+  constructor(private messageService: MessageService) {
+  }
+
+  openAboutWindow() {
+    this.messageService.sendMessage(MESSAGE_TYPE.OPEN_ABOUT_WINDOW);
+  }
 }
