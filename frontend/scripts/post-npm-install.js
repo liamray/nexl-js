5df45a2f0c0c30898ee2ecd37753f01989d3366c
liamray <liamraymisc@gmail.com>
2018-10-06 13:09:14 +0300

    - fixed build

--- frontend/scripts/post-npm-install.js
+++ frontend/scripts/post-npm-install.js
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// this script import winston's log levels and storing in file in frontend ( unfortunately it's impossible to import that directly )
+// importing winston's log levels and storing in file in frontend ( unfortunately it's impossible to import that directly )
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 const WINSTON_LOG_LEVELS = './src/app/main/common/winston-log-levels.json';
@@ -12,3 +12,20 @@ const winston = require('winston');
 const fs = require('fs');
 
 fs.writeFileSync(WINSTON_LOG_LEVELS, JSON.stringify(winston.levels, null, 2));
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// copying a licensed version of jqxWidgets to node_modules
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const fse = require('fs-extra');
+
+const srcDir = './jqwidgets';
+const destDir = './node_modules/jqwidgets-scripts';
+
+console.log('-----------------------------------------------------------------------------------------------------------------');
+console.log(`Copying a [${srcDir}] licensed version to [${destDir}] directory`);
+console.log('-----------------------------------------------------------------------------------------------------------------');
+
+fse.remove(destDir)
+  .then(_ => fse.ensureDir(destDir))
+  .then(_ => fse.copy(srcDir, destDir));
